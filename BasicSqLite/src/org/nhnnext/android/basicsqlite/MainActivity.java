712500acb5a9f4f1053dd2ae7c598c3dec14159a
flashscope <flashscope@limecolor.com>
2013-09-02 12:05:11 +0900

    사용안하는 import삭제

--- BasicSqLite/src/org/nhnnext/android/basicsqlite/MainActivity.java
+++ BasicSqLite/src/org/nhnnext/android/basicsqlite/MainActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
